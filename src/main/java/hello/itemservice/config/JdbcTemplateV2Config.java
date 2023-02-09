4e9e464cbcb019668f170c4cec9aa76b45d6fe96
jiwon615 <seg615@naver.com>
2023-02-09 20:36:32 +0900

    feat: NamedParameterJdbcTemplate 사용

--- src/main/java/hello/itemservice/config/JdbcTemplateV2Config.java
+++ src/main/java/hello/itemservice/config/JdbcTemplateV2Config.java
@@ -1,15 +1,20 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.memory.MemoryItemRepository;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV2;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
-public class MemoryConfig {
+@RequiredArgsConstructor
+public class JdbcTemplateV2Config {
+
+    private final DataSource dataSource;
 
     @Bean
     public ItemService itemService() {
@@ -18,6 +23,7 @@ public class MemoryConfig {
 
     @Bean
     public ItemRepository itemRepository() {
-        return new MemoryItemRepository();
+        // ItemRepository 구현체로 JdbcTemplateItemRepositoryV1이 사용되도록 했음
+        return new JdbcTemplateItemRepositoryV2(dataSource);
     }
 }
--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * JdbcTemplate + e
+ * JdbcTemplate + 동적 쿼리
  */
 @Slf4j
 @Repository
